f82a9cf9fe8e1732379647c7912418b2beec4467
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-02-27 15:14:53 +0500

    config file added

---
+++ class/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 205
+ * Date: 2/27/2016
+ * Time: 3:10 PM
+ */
+
+define('DB_NAME', 'sql6108017');
+define('DB_USER', 'sql6108017');
+define('DB_PASSWORD', 'qy4WKEwXIR');
+define('DB_HOST', '"sql6.freemysqlhosting.net"');
+
+define('TITLE', 'some title');
+
+?>
+
+
